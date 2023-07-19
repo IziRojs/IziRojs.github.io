ab1eeb024f3d91f926415b901340bd992ec10560
IziRojs <139879027+IziRojs@users.noreply.github.com>
2023-07-19 22:08:38 +0200

    3 nested datumi
    random datumi, 2 layerja, urejen class

--- script.js
+++ script.js
@@ -12,43 +12,47 @@ function createButtons() {
   // Remove the "Click Me" button
   parentElement.removeChild(clickMeButton);
 
-  // Create the new buttons
-  const service1Button = createServiceButton("Service 1", "03.08");
-  const service2Button = createServiceButton("Service 2", "04.08");
+  // Create Service 1 button
+  const service1Button = document.createElement("button");
+  service1Button.innerText = "Service 1";
+  service1Button.classList.add("service-button");
+  parentElement.appendChild(service1Button);
 
+  // Create Service 2 button
+  const service2Button = document.createElement("button");
+  service2Button.innerText = "Service 2";
+  service2Button.classList.add("service-button");
+  parentElement.appendChild(service2Button);
+
+  // Add click event listeners to Service 1 and Service 2 buttons
   service1Button.addEventListener("click", function() {
-    createThirdLayer(service1Button);
+    createNestedButtons("Service 1");
   });
 
   service2Button.addEventListener("click", function() {
-    createThirdLayer(service2Button);
+    createNestedButtons("Service 2");
   });
 }
 
-function createServiceButton(serviceName, date) {
+function createNestedButtons(serviceName) {
   const parentElement = document.body;
 
-  // Create the service button
-  const serviceButton = document.createElement("button");
-  serviceButton.innerHTML = `<div><!----> ${date} </div><div>18:30</div><div><b> Čez 15 dni </b></div>`;
-  serviceButton.classList.add("btn", "service");
-  serviceButton.textContent = serviceName;
-  parentElement.appendChild(serviceButton);
+  // Remove the Service buttons
+  const serviceButtons = document.querySelectorAll(".service-button");
+  serviceButtons.forEach(button => {
+    parentElement.removeChild(button);
+  });
 
-  return serviceButton;
+// Create 3 nested buttons with unique dates
+for (let i = 0; i < 3; i++) {
+  const button = document.createElement("button");
+  const currentDate = new Date();
+  const futureDate = new Date();
+  futureDate.setDate(currentDate.getDate() + (i * 15)); // Add 15 days to the current date
+
+  const buttonDate = futureDate.toLocaleDateString();
+  button.innerHTML = `<div>${buttonDate}</div><div>18:30</div><div><b>Čez 15 dni</b></div>`;
+  button.classList.add("btn", "m--font-inverse-light", "btn-outline-success", "btn-sm", "mr-2", "mb-3");
+  parentElement.appendChild(button);
 }
-
-function createThirdLayer(serviceButton) {
-  const parentElement = document.body;
-
-  // Remove the service button
-  parentElement.removeChild(serviceButton);
-
-  // Create the third layer buttons
-  for (let i = 1; i <= 3; i++) {
-    const dateButton = document.createElement("button");
-    dateButton.textContent = "Date " + i;
-    dateButton.classList.add("time");
-    parentElement.appendChild(dateButton);
-  }
 }
