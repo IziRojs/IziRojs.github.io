1ae6be90151013bad636990face3272be50fd4a4
IziRojs <139879027+IziRojs@users.noreply.github.com>
2023-07-19 14:05:51 +0200

    Začasna sprememba kode
    / priprava da je podobna MF kodi

--- script.js
+++ script.js
@@ -14,15 +14,17 @@ function createButtons() {
 
   // Create the new buttons
   const service1Button = document.createElement("button");
-  service1Button.textContent = "Service 1";
+  service1Button.innerHTML = '<div><!----> čet. 03.08 </div><div>18:30</div><div><b> Čez 15 dni </b></div>';
   service1Button.id = "service-button-1";
-  service1Button.classList.add("service");
+  service1Button.classList.add("btn", "m--font-inverse-light", "btn-outline-success", "btn-sm", "mr-2", "mb-3");
+  service1Button.disabled = true;
   parentElement.appendChild(service1Button);
 
   const service2Button = document.createElement("button");
-  service2Button.textContent = "Service 2";
+  service2Button.innerHTML = '<div><!----> čet. 03.08 </div><div>18:30</div><div><b> Čez 15 dni </b></div>';
   service2Button.id = "service-button-2";
-  service2Button.classList.add("service");
+  service2Button.classList.add("btn", "m--font-inverse-light", "btn-outline-success", "btn-sm", "mr-2", "mb-3");
+  service2Button.disabled = true;
   parentElement.appendChild(service2Button);
 
   service1Button.addEventListener("click", function() {
@@ -45,7 +47,7 @@ function createThirdLayer(serviceButtonId) {
   for (let i = 1; i <= 3; i++) {
     const dateButton = document.createElement("button");
     dateButton.textContent = "Date " + i;
-    dateButton.id = "date-button-" + i;
+    dateButton.id = "date-button";
     dateButton.classList.add("time");
     parentElement.appendChild(dateButton);
   }
